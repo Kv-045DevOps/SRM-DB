b0ad9298384fdcc320745339c92871e92cc0a595
a.kubrachenko <artemkubrachenko@gmail.com>
2019-01-16 21:31:07 +0200

    change sh 1

--- init-container/check_dump.sh
+++ init-container/check_dump.sh
@@ -1,4 +1,4 @@
-until pg_isready -h srmsystemd -p 5432; do echo waiting for database; sleep 2; done;
+until pg_isready -h srmsystemdb -p 5432; do echo waiting for database; sleep 2; done;
 export SELECT=$(psql -t -h srmsystemdb -p 5432 -c "select 1 from information_schema.tables where table_schema='public' and table_name='department'")
 if [ $SELECT = 1 ]
 then
