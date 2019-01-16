dfe8a5fd5625bff3c42610d2f56b7c6b910e4464
a.kubrachenko <artemkubrachenko@gmail.com>
2019-01-16 17:08:05 +0200

    change dest

--- init-container/check_dump.sh
+++ init-container/check_dump.sh
@@ -1,8 +1,8 @@
-export SELECT=$(psql -t -h localhost -p 5432 -c "select 1 from information_schema.tables where table_schema='public' and table_name='department'")
+export SELECT=$(psql -t -h srmsystemdb -p 5432 -c "select 1 from information_schema.tables where table_schema='public' and table_name='department'")
 if [ $SELECT -eq 1 ]
 then
     exit 0
 else
-    psql -h localhost -p 5432 -f ~/tmp/dbdump
+    psql -h srmsystemdb -p 5432 -f ~/tmp/dbdump
     exit 0
 fi
