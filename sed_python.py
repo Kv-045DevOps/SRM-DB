61ac3e35055a6737727c30208e2b34103e3a1d26
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-23 04:38:53 -0500

    Change SED replace script

--- sed_python.py
+++ sed_python.py
@@ -5,10 +5,12 @@ with open(sys.argv[1], 'r') as file:
     str_tmp = "ghostgoose"
     data = file.read()
     tmp = sys.argv[2] + ":" + sys.argv[3]
-    data = data.replace("100.71.71.71:5000/db-service:latest", tmp)
-    data_init = data.replace("100.71.71.71:5000/init-container:latest", tmp)
-    print(data_init)
+    if sys.argv[2] == "100.71.71.71:5000/db-service":
+        data = data.replace("100.71.71.71:5000/db-service:latest", tmp)
+    elif sys.argv[2] == "100.71.71.71:5000/init-container":
+        data = data.replace("100.71.71.71:5000/init-container:latest", tmp)
+    print(data)
          
 
 with open(sys.argv[1], 'w') as file:
-    file.write( data_init )
+    file.write( data )
