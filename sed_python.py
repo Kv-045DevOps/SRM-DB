e3cf2a85bf69eb5638764ff50bc215981c7e6129
Maxghost33 <maxim.jovanik@gmail.com>
2019-01-23 04:26:35 -0500

    Change Jenkinsfile

--- sed_python.py
+++ sed_python.py
@@ -5,9 +5,10 @@ with open(sys.argv[1], 'r') as file:
     str_tmp = "ghostgoose"
     data = file.read()
     tmp = sys.argv[2] + ":" + sys.argv[3]
-    data = data.replace("100.71.71.71:5000/get-service:v2", tmp)
-    print(data)
+    data = data.replace("100.71.71.71:5000/db-service:latest", tmp)
+    data_init = data.replace("100.71.71.71:5000/init-container:latest", tmp)
+    print(data_init)
          
 
 with open(sys.argv[1], 'w') as file:
-    file.write( data )
+    file.write( data_init )
