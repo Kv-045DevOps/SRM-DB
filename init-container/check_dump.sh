455f0fd445e783a169b499484dfb4b4d8832901e
a.kubrachenko <artemkubrachenko@gmail.com>
2019-02-11 20:08:25 +0200

    for demo 4

--- init-container/check_dump.sh
+++ init-container/check_dump.sh
@@ -1,9 +1,9 @@
-until pg_isready -h srmsystemdb -p 5432; do echo waiting for database; sleep 2; done;
-export SELECT=$(psql -t -h srmsystemdb -p 5432 -c "select 1 from information_schema.tables where table_schema='public' and table_name='department'")
+until pg_isready; do echo waiting for database; sleep 2; done;
+export SELECT=$(psql -t -c "select 1 from information_schema.tables where table_schema='public' and table_name='department'")
 if [ $SELECT = 1 ]
 then
     exit 0
 else
-    psql -h srmsystemdb -p 5432 -f dbdump
+    psql -f dbdump
     exit 0
 fi
